ef6abd464f42d8621afef027f7a17f65478c0a55
Hj Wu <black99129@gmail.com>
2022-06-19 22:38:32 +0800

    dev

--- init.el
+++ init.el
@@ -14,12 +14,20 @@
 ;; Emacs-Neotree
 ;; --------------------------
 (require 'init-neotree)
+;; Emacs-base
+;; --------------------------
+(require 'init-base)
+;; Emacs-dev
+;; --------------------------
+(require 'init-dev)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(neotree use-package evil)))
+ '(package-selected-packages
+   '(markdown-mode yasnippet posframe neotree use-package evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ lisp/init-base.el
@@ -0,0 +1,9 @@
+;; MacOS specific
+(use-package exec-path-from-shell
+  :ensure t
+  :when (eq system-type 'darwin)
+  :hook (after-init . exec-path-from-shell-initialize))
+
+(setq make-backup-files nil)
+
+(provide 'init-base)
