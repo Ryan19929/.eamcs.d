a198616303cf21fe3335f31fad158ee201466a08
Hj Wu <black99129@gmail..com>
2022-06-25 20:06:38 +0800

    delete evil and neotree

--- init.el
+++ init.el
@@ -6,13 +6,6 @@
 ;; Emacs-Ui
 ;; --------------------------
 (require 'init-ui)
-
-;; Emacs-Evil
-;; --------------------------
-(require 'init-evil)
-;; Emacs-Neotree
-;; --------------------------
-(require 'init-neotree)
 ;; Emacs-base
 ;; --------------------------
 (require 'init-base)
@@ -28,7 +21,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(pyim dashboard projectile all-the-icons evil-org markdown-mode yasnippet posframe neotree use-package evil)))
+   '(edwina vterm pyim dashboard projectile all-the-icons evil-org markdown-mode yasnippet posframe neotree use-package evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
