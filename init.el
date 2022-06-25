7b973308875e03d73ecaf3589fa6fbaa827994b1
Hj Wu <black99129@gmail..com>
2022-06-25 20:20:06 +0800

    delete file

--- init.el
+++ init.el
@@ -15,6 +15,9 @@
 ;; Emacs-org
 ;; --------------------------
 (require 'init-org)
+;; Emacs-keymap
+;; --------------------------
+(require 'init-keymap)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ lisp/init-keymap.el
@@ -0,0 +1,8 @@
+;; which-key
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+
+(provide 'init-keymap)
