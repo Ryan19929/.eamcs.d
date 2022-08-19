a9640275782706d36ef5207b0adc95413351a4db
Hj Wu <black99129@gmail..com>
2022-08-19 09:44:13 +0800

    org-mode 自动换行

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -3,6 +3,7 @@
       (quote ((sequence "TODO(t)" "STARTED(s)" "|" "DONE(d!/!)")
 	      (sequence "WAITING(w@/!)" "SOMEDAY(S)" "|" "CANCELLED(c@/!)" "MEETING(m)" "PHONE(p)"))))
 
+(add-hook 'org-mode-hook (lambda ()(setq truncate-lines nil)))
 ;;fix doom modeline
 
 
---
+++ lisp/init-plug.el
@@ -0,0 +1,4 @@
+(setq bar nil)
+
+(dolist (foo (list "*" "** 当日" "** 正文" "** 今日所见之事"))
+  (message foo))
